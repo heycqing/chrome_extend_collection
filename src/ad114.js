a103309c064a246e353b0d0b86c0e9e0efa651d3
wusiqing <siqingiii@163.com>
2019-10-20 23:10:17 +0800

    更新 防止114广告的脚本

--- src/ad114.js
+++ src/ad114.js
@@ -1,30 +1,48 @@
 (function () {
     function stopAD(){
         // 针对 114中文阅读网的
-        // var adNode = document.getElementsByClassName('afd-ad')
-        var adNode = document.getElementById('_qlt4qo12rut')
-        var adBottomNode = document.getElementById('cZ115713822152679')
-        var ad_3 = document.getElementsByClassName('mb10')
-        for (var i = 0; i < adNode.length; i++) {
-            adNode[i].style.display = 'none';
+        var tempBody = document.getElementsByTagName('body')[0]
+        // 顶部的广告
+        var adTopNode = document.getElementById('content')
+        var adTop_node1 = adTopNode.firstElementChild.nextElementSibling
+        var adTop_node2 = adTop_node1.nextElementSibling.nextElementSibling
+
+        // 底部的广告
+        var adBottomNode = document.getElementsByClassName('mb10')
+        // 悬浮的广告
+        // 记得 script 标签在最后面
+        var node1 = tempBody.lastElementChild
+        var node2 = node1.previousElementSibling
+        
+      
+
+        for(var i = 0; i < adBottomNode.length; i++){
+            adBottomNode[i].style.display = 'none'
         }
-        for (var j = 0; j < adBottomNode.length; j++) {
-            adBottomNode[j].style.display = 'none'
+
+        if(node1 != null && node2 != null){
+            tempBody.removeChild(node1)
+            tempBody.removeChild(node2)
         }
-        for (var k = 0; k < ad_3.length; k++) {
-            bottomOperateTop[k].style.display = 'none'
-        }
+
+        if(adTop_node1 != null && adTop_node2 != null){
+            adTopNode.removeChild(adTop_node1)
+            adTopNode.removeChild(adTop_node2)
+        } 
+
+
     }
 
     window.addEventListener('onload', function(e){
         stopAD()
     })
 
-    // 监听页面高度变化
-    window.addEventListener('scroll', function (e) {
-        stopAD()        
-    })
-
     stopAD()
 
 })()
+
+  // var adFloat_a_Node = document.evaluate("//a[@style='background-color: rgba(255, 255, 255, 0) !important; height: 237px;']",document, null, XPathResult.ANY_TYPE, null )
+
+        // var just_adFloatNode = adFloatNode.iterateNext()
+        // var just_adFloat_a_Node = adFloat_a_Node.iterateNext()
+        // var just_adFloat_other_Node = adFloat_other_Node.iterateNext()
